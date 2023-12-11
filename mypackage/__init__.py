06ec942daaa10d021dad74347d78035900369cfa
Bottone, Davide <davide.bottone@empa.ch>
2023-12-11 10:38:45 +0100

    Added pre-commit CI

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,5 +1,2 @@
-from .geometry import Point, Line
-from .utils import distance
-
 def personal_message():
     print("Hello from John Doe!")
--- mypackage/utils.py
+++ mypackage/utils.py
@@ -1,4 +1,6 @@
-import math
+import numpy as np
+from geometry import Point
+
 
 def distance(p1: "Point", p2: "Point") -> float:
-    return ((p1.x-p2.x)**2 + (p1.y-p2.y)**2)**0.5  
+    return np.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
